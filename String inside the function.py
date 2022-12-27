fefc27e006acf0f17b3059affef2c34ad50b37e0
Wafeeq Izath <120131070+ezath97@users.noreply.github.com>
2022-12-27 07:46:34 +0530

    Question 2

---
+++ String inside the function.py
@@ -0,0 +1,15 @@
+# Write a Python program to reverse a string.
+
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
+
+
+def reverse(s):
+    return s[::-1]
+    
+string = "1234abcd"
+
+Output = reverse(string)
+print('Sample string', string)
+print('Expected Output', Output)
+   
